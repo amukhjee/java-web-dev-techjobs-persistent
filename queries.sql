3ed9d9f0017ffbfa96bd78d6f1ebe2266f1e3a6d
amukhjee <amukhjee1@gmail.com>
2020-03-17 16:59:18 -0500

    SQL TASK DONE

--- queries.sql
+++ queries.sql
@@ -1,7 +1,9 @@
 ## Part 1: Test it with SQL
-
+SELECT * FROM techjobs.job;
 ## Part 2: Test it with SQL
-
+SELECT name FROM techjobs.employer WHERE location="St. Louis City";
 ## Part 3: Test it with SQL
-
+DROP TABLE techjobs.job;
 ## Part 4: Test it with SQL
+SELECT DISTINCT name, description FROM techjobs.skill
+INNER JOIN techjobs.job_skills ON techjobs.skill.id=techjobs.job_skills.skills_id ORDER BY name;
